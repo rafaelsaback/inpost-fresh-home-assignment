e679207a98825ecb066c6e7b100549ee46ff6259
Rafael Saback <rafael@rollup.ai>
2024-12-09 11:36:26 +0100

    Extract sorting logic to separate function

--- src/task.ts
+++ src/task.ts
@@ -20,6 +20,49 @@ const isStringANumber = (strNumber: string): boolean => {
   return !isNaN(parseInt(strNumber));
 };
 
+const sortElements = (category: Category, fallBackTitle: string) => {
+  const order = getOrder(category.Title);
+  const orderL1 = isStringANumber(order) ? parseInt(order) : category.id;
+
+  const l2Kids = (category.children ?? []).map((c2) => {
+    const order2 =
+      c2.Title && c2.Title.includes('#') ? getOrder(c2.Title) : fallBackTitle;
+    const orderL2 = isStringANumber(order2) ? parseInt(order2) : c2.id;
+
+    const l3Kids = (c2.children ?? []).map((c3) => {
+      const order3 =
+        c3.Title && c3.Title.includes('#') ? getOrder(c3.Title) : fallBackTitle;
+      const orderL3 = isStringANumber(order3) ? parseInt(order3) : c3.id;
+      return {
+        id: c3.id,
+        image: c3.MetaTagDescription,
+        name: c3.name,
+        order: orderL3,
+        children: [],
+        showOnHome: false,
+      };
+    });
+    l3Kids.sort((a, b) => a.order - b.order);
+    return {
+      id: c2.id,
+      image: c2.MetaTagDescription,
+      name: c2.name,
+      order: orderL2,
+      children: l3Kids,
+      showOnHome: false,
+    };
+  });
+  l2Kids.sort((a, b) => a.order - b.order);
+  return {
+    id: category.id,
+    image: category.MetaTagDescription,
+    name: category.name,
+    order: orderL1,
+    children: l2Kids,
+    showOnHome: false,
+  };
+};
+
 export const getCategoryTree = async (
   getCategories: () => Promise<{ data: Category[] }>
 ): Promise<CategoryListElement[]> => {
@@ -32,50 +75,10 @@ export const getCategoryTree = async (
   const toShowOnHome: number[] = [];
 
   const result = res.data.map((c1) => {
-    const order = getOrder(c1.Title);
-    const orderL1 = isStringANumber(order) ? parseInt(order) : c1.id;
-
     if (c1.Title.includes('#')) {
       toShowOnHome.push(c1.id);
     }
-
-    const l2Kids = (c1.children ?? []).map((c2) => {
-      const order2 =
-        c2.Title && c2.Title.includes('#') ? getOrder(c2.Title) : c1.Title;
-      const orderL2 = isStringANumber(order2) ? parseInt(order2) : c2.id;
-
-      const l3Kids = (c2.children ?? []).map((c3) => {
-        const order3 =
-          c3.Title && c3.Title.includes('#') ? getOrder(c3.Title) : c1.Title;
-        const orderL3 = isStringANumber(order3) ? parseInt(order3) : c3.id;
-        return {
-          id: c3.id,
-          image: c3.MetaTagDescription,
-          name: c3.name,
-          order: orderL3,
-          children: [],
-          showOnHome: false,
-        };
-      });
-      l3Kids.sort((a, b) => a.order - b.order);
-      return {
-        id: c2.id,
-        image: c2.MetaTagDescription,
-        name: c2.name,
-        order: orderL2,
-        children: l3Kids,
-        showOnHome: false,
-      };
-    });
-    l2Kids.sort((a, b) => a.order - b.order);
-    return {
-      id: c1.id,
-      image: c1.MetaTagDescription,
-      name: c1.name,
-      order: orderL1,
-      children: l2Kids,
-      showOnHome: false,
-    };
+    return sortElements(c1, c1.Title);
   });
 
   result.sort((a, b) => a.order - b.order);
